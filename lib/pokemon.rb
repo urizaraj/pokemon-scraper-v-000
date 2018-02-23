43af2c67ba9325443c95c6c39d6c08186526c2b0
Alexander Urizar <urizaraj@gmail.com>
2018-02-23 19:19:41 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,5 @@
 class Pokemon
-  attr_accessor :name, :type, :db, :id
+  attr_accessor :name, :type, :db, :id, :hp
 
   @@all = []
 
@@ -25,6 +25,6 @@ class Pokemon
   end
 
   def alter_hp(power)
-    
+
   end
 end
